34bd7d589cbb70de560c669be2304389efc6b898
Nelson Lamprecht <Sorbitol77@outlook.com>
2022-08-18 21:58:21 -0500

    initial

---
+++ meadow-cyclecolors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+
+using Meadow;
+
+namespace Meadow_CycleColors
+{
+    internal class Program
+    {
+        static IApp _app;
+        public static void Main(string[] args)
+        {
+            if (args.Length > 0 && args[0] == "--exitOnDebug") return;
+
+            // instantiate and run new meadow app
+            _app = new MeadowApp();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
